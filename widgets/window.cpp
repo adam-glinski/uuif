edb9ee8dafec91d7cc07c9b9b49ec878c8aaed76
adam-glinski <adam.s.glinski@gmail.com>
2023-05-13 20:16:13 +0200

    repo init
    added:

---
+++ widgets/window.cpp
@@ -0,0 +1,25 @@
+#include "window.h"
+#include "../core/core.h"
+#include "../utils/hashing.hpp"
+
+uuif::c_window::~c_window()
+{
+    id_t window_id = g_hash->compile_time(this->get_label().c_str());
+    core->remove_window(window_id);
+}
+
+void uuif::c_window::draw()
+{
+    return;
+}
+
+void uuif::c_window::update()
+{
+    return;
+}
+
+void uuif::c_window::input()
+{
+    return;
+}
+
