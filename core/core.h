4ee6c2297d1b2173bec67fe6406c2cf4348a5de7
adam-glinski <adam.s.glinski@gmail.com>
2023-05-26 21:28:37 +0200

    wip

--- core/core.h
+++ core/core.h
@@ -1,15 +1,21 @@
 #pragma once
-#include "../utils/singleton.hpp"
-#include "../data_types.h"
+
 #include <unordered_map>
 #include <memory>
 #include <string>
 
-namespace uuif {
-    class c_window;
-    class c_core : public c_singleton <c_core>
+#include "../utils/singleton.hpp"
+#include "../data_types.h"
+
+#include "../widgets/widget.h"
+#include "../widgets/window.h"
+
+namespace uuif
+{
+    class c_core : public c_singleton<c_core>
     {
-        std::unordered_map <id_t, std::shared_ptr<c_window>> m_windows{};
+        std::unordered_map<id_t, std::shared_ptr<c_window>> m_windows{};
+
     public:
         std::shared_ptr<c_window> add_window(std::string title, position_t position, dimension_t size);
         bool remove_window(id_t id);
--- widgets/widget.h
+++ widgets/widget.h
@@ -1,17 +1,20 @@
+#pragma once
 #include <memory>
 #include <string>
 
 #include "../data_types.h"
 
-
-namespace uuif {
-    class c_widget : public std::enable_shared_from_this<c_widget> {
+namespace uuif
+{
+    class c_widget : public std::enable_shared_from_this<c_widget>
+    {
     private:
         position_t m_position{};
         dimension_t m_size{};
         std::string m_label{};
         std::shared_ptr<c_widget> m_parent{};
         widget_t m_type = widget_t::base;
+
     public:
         void set_size(dimension_t size);
         dimension_t get_size();
@@ -28,7 +31,6 @@ namespace uuif {
         virtual void draw() = 0;
         virtual void update() = 0;
         virtual void input() = 0;
-
 
         // for hashmap
     };
